0db4978c06e0c6e5b3d41beb04c2d1773cedbcf3
SnappySM <nicola.scognamillo@estudiantat.upc.edu>
2024-11-30 16:09:03 +0100

    Mostrar imatge seleccionada.

--- src/app/achievement-creation/achievement-creation.component.ts
+++ src/app/achievement-creation/achievement-creation.component.ts
@@ -5,34 +5,42 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {NgIf, NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-achievement-creation',
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule, MatCardModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, ReactiveFormsModule, MatCardModule, MatButtonModule, MatIconModule, NgIf],
   templateUrl: './achievement-creation.component.html',
   standalone: true,
   styleUrl: './achievement-creation.component.css'
 })
 export class AchievementCreationComponent {
-  private srcResult: any;
+  protected imageUrl: any;
   protected selectedFile: any;
   form: FormGroup = new FormGroup({
     name: new FormControl(),
     category: new FormControl(),
   });
 
-  protected onFileSelected(){
-    const inputNode: any = document.querySelector('#file');
-
-    if (typeof (FileReader) !== 'undefined') {
-      const reader = new FileReader();
-
-      reader.onload = (e: any) => {
-        this.srcResult = e.target.result;
-      };
-      this.selectedFile = inputNode.files[0] ?? null;
-      reader.readAsArrayBuffer(inputNode.files[0]);
+  protected onFileSelected(event: Event){
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0){
+      const file = input.files![0];
+      if (file.type.startsWith('image/')){
+        this.selectedFile = file;
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.imageUrl = e.target.result;
+        }
+        reader.readAsDataURL(file);
+      }
     }
+  }
+
+  protected deleteFile(){
+    this.imageUrl = null;
+    this.selectedFile = null;
   }
 
   protected onSubmit(){
